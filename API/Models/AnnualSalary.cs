00cc0a7738c67ff3e2dc4c2cf4d84a94ec1f1d01
Will Weinzettl <will.weinzettl@number41media.com>
2021-01-22 16:51:46 -0800

    Changed annual salary json property names

--- API/Models/AnnualSalary.cs
+++ API/Models/AnnualSalary.cs
@@ -1,10 +1,13 @@
-﻿using System;
+﻿using System.Text.Json.Serialization;
 
 namespace SearchAllOccupationsToolAPI.Models
 {
     public class AnnualSalary
     {
+        [JsonPropertyName("id")]
         public int Id { get; set; }
+
+        [JsonPropertyName("value")]
         public string SalaryText { get; set; }
     }
 }
