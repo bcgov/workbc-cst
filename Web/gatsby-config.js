ae9745874ecf0768f7b69e4d80830f0c1b5e4053
Chowdi <chowdi.appasani@cgi.com>
2021-02-03 16:44:44 -0800

    fix build - remove unnecessary plugin

--- Web/gatsby-config.js
+++ Web/gatsby-config.js
@@ -35,7 +35,7 @@ module.exports = {
         //trackingId: `ADD YOUR TRACKING ID HERE`,
       },
     },
-    `gatsby-plugin-feed`,
+    // `gatsby-plugin-feed`,
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-typescript`,
     `gatsby-plugin-antd`,
