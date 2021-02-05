76846157631b85fa59719d7fbc18f02014578e82
Chowdi <chowdi.appasani@cgi.com>
2021-02-04 21:19:24 -0800

    Display profile image and route to compare careers

--- Web/sao/src/components/compareCareers.tsx
+++ Web/sao/src/components/compareCareers.tsx
@@ -1,28 +1,16 @@
-import React, {FunctionComponent, useEffect, useState} from 'react'
+import React, {FunctionComponent} from 'react'
 import {Button} from 'antd'
-import {navigate} from 'gatsby'
-import {useFilterContext} from '../state/filterContext'
-import {FilterOptionModel} from '../client/dataTypes'
+import {useHistory} from 'react-router-dom'
 
 const CompareCareers: FunctionComponent = () => {
-    const {filterOption, filteredOccupationsList, selectedNoc, setSelectedNoc, setFilteredOccupationsList} = useFilterContext()
+    const history = useHistory()
 
     function handleReturn() {
-        navigate('/')
+        history.push('/')
     }
-
-    const [filterParam, setFilterParams] = useState<FilterOptionModel>()
-
-    useEffect(() => {
-        if (!!filterOption) {
-                console.log(`latest filter params: ${JSON.stringify(filterOption)}`)
-                setFilterParams(filterOption)
-            }
-        },[filterOption])
 
     return(
         <div>
-            {/* <b> From context, Selected Filter Options : {JSON.stringify(filterOption)} </b> */}
             <h1>Compare Careers</h1>
             <Button onClick={handleReturn}> Home page </Button>
         </div>
--- Web/sao/src/pages/index.tsx
+++ Web/sao/src/pages/index.tsx
@@ -1,13 +1,27 @@
 import React, {FunctionComponent} from "react"
+import {Route, BrowserRouter, Switch} from 'react-router-dom' 
 import 'antd/dist/antd.css'
 import Dropdowns from '../components/dropdowns'
+import NotFoundPage from '../components/404'
 import {FilterContextProvider} from '../state/filterContext'
+import CompareCareers from "../components/compareCareers"
 
 const CareerSearchToolIndex:FunctionComponent = () => {
-  
   return (
     <FilterContextProvider>
-      <Dropdowns />
+      <BrowserRouter>
+        <Switch>
+            <Route exact path="/">
+              <Dropdowns />
+            </Route>
+            <Route exact path="/compareCareers">
+              <CompareCareers />
+            </Route>
+            <Route path="*">
+              <NotFoundPage />
+            </Route>
+          </Switch>
+      </BrowserRouter>
     </FilterContextProvider>
     )
 }
