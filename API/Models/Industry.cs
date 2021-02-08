0e0d6b94c820c51043e368a3f4be8f1d7b32a370
Will Weinzettl <will.weinzettl@number41media.com>
2021-02-08 12:07:26 -0800

    Added Fake Subindustry lists within Industries action

--- API/Models/Industry.cs
+++ API/Models/Industry.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Text.Json.Serialization;
 
@@ -13,5 +15,21 @@ namespace SearchAllOccupationsToolAPI.Models
 
         [JsonPropertyName("value")]
         public string Value { get; set; }
+
+        [NotMapped]
+        public List<SubIndustry> SubIndustries { get; set; }  // Use Collection
+    }
+
+    public class SubIndustry
+    {
+        [JsonPropertyName("id")]
+        public int Id { get; set; }
+
+        [JsonPropertyName("value")]
+        public string Value { get; set; }
+
+        [NotMapped]
+        [JsonIgnore]
+        public virtual Industry Industry { get; set; }
     }
 }
--- API/Repositories/IndustryRepository.cs
+++ API/Repositories/IndustryRepository.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
 using SearchAllOccupationsToolAPI.DbContexts;
 using SearchAllOccupationsToolAPI.DbContexts.Interfaces;
 using SearchAllOccupationsToolAPI.Models;
@@ -19,11 +20,108 @@ namespace SearchAllOccupationsToolAPI.Repositories
         public List<Industry> GetIndustries()
         {
             if (_context.IsSQLServer)
-                return _context.Industries
+            {
+                var industries = _context.Industries
                     .Where(i => i.Value != "All industries")
                     .ToList();
-            
+
+                var subIndustryIndex = 0;
+
+                var fakeSubs = FakeSubs;
+                
+                foreach (var industry in industries)
+                {
+                    industry.SubIndustries = new List<SubIndustry>();
+                    for (var i = 0; i < 4 ; i++)
+                    {
+                        if (subIndustryIndex == fakeSubs.Count)
+                            subIndustryIndex = 0;
+
+                        industry.SubIndustries.Add(
+                            new SubIndustry
+                            {
+                                Id = subIndustryIndex + 1,
+                                Value = fakeSubs.ElementAtOrDefault(subIndustryIndex)
+                            }
+                        );
+                        subIndustryIndex++;
+                    }
+                }
+
+                return industries;
+            }
+
             return ContextHelper.GetPlaceHolderData<Industry>("SampleJsonFiles/industries.json");
         }
+
+        private List<string> FakeSubs = new List<string>
+        {
+"Accommodation services",
+"Air transportation",
+"Ambulatory health care services",
+"Amusement, gambling and recreation industries",
+"Architectural, engineering and related services",
+"Broadcasting, data processing, and information",
+"Business, building and other support services",
+"Community colleges",
+"Computer systems design and related services",
+"Construction",
+"Elementary and secondary schools",
+"Fabricated metal product manufacturing",
+"Farms",
+"Federal government public administration",
+"Finance",
+"Fishing, hunting and trapping",
+"Food services and drinking places",
+"Food, beverage and tobacco manufacturing",
+"Forestry and logging",
+"Health and personal care stores",
+"Heritage institutions",
+"Hospitals",
+"Industry",
+"Insurance carriers and related activities",
+"Legal, accounting, design, research, and advertising services",
+"Local and Indigenous public administration",
+"Machinery manufacturing",
+"Management, scientific and technical consulting services",
+"Mining     ",
+"Motion picture and sound recording industries",
+"Motor vehicle and parts dealers",
+"Nursing and residential care facilities",
+"Oil and gas extraction",
+"Other manufacturing",
+"Other retail trade (excluding cars and personal care)",
+"Paper manufacturing",
+"Performing arts, spectator sports and related industries",
+"Postal service, couriers and messengers",
+"Primary metal manufacturing",
+"Private and trades education",
+"Provincial and territorial public administration",
+"Publishing industries",
+"Rail transportation",
+"Real estate rental and leasing",
+"Repair, personal and non-profit services",
+"Ship and boat building",
+"Social assistance",
+"Support activities for agriculture and forestry",
+"Support activities for mining and oil and gas extraction",
+"Support activities for transportation",
+"Telecommunications",
+"Transit, sightseeing, and pipeline transportation",
+"Transportation equipment manufacturing (excluding shipbuilding)",
+"Truck transportation",
+"Universities",
+"Utilities",
+"Warehousing and storage",
+"Water transportation",
+"Wholesale trade",
+"Wood product manufacturing",
+
+        };
+
+        
     }
+
+    
+
 }
