d707953b168fec4c2ae8d4506bf90244c95879b5
Chowdi <chowdi.appasani@cgi.com>
2021-02-25 12:45:32 -0800

    test env api configuration

--- Web/sao/src/client/httpClient.ts
+++ Web/sao/src/client/httpClient.ts
@@ -1,13 +1,13 @@
-// import axios from 'axios'
+import axios from 'axios'
 
-// import { API_URL } from './config'
+import { API_URL } from './config'
 
-// export function configureHttpClient() {
-//   let baseURL: string
-//   if (API_URL) baseURL = `${API_URL}`
-//   else baseURL = '/v1' // fallback
+export function configureHttpClient() {
+  let baseURL: string
+  if (API_URL) baseURL = `${API_URL}`
+  else baseURL = '/v1' // fallback
 
-//   axios.defaults.headers['Content-Type'] = 'application/json'
+  axios.defaults.headers['Content-Type'] = 'application/json'
 
-//   axios.defaults.baseURL = baseURL
-// }
+  axios.defaults.baseURL = baseURL
+}
