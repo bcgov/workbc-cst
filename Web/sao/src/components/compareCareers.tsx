1339bd4bfa48013a7f8a5f30431db92158296f40
Chowdi <chowdi.appasani@cgi.com>
2021-03-05 14:46:29 -0800

    stop playing un related videos from career preview and compare careers

--- Web/sao/src/components/compareCareers.tsx
+++ Web/sao/src/components/compareCareers.tsx
@@ -102,7 +102,7 @@ const CompareCareers: FunctionComponent = () => {
                 </div>
                 <div  className="result-detail__thumbnail">
                     {(careerObj.careerDetail?.careertrekvideoids.length === 0) ? (<img src={profileImagesPath+getProfileImageName(careerObj.nocId)} alt='career profile pic'/>)
-                    : (<YouTube videoId={careerObj.careerDetail?.careertrekvideoids[0]} opts={{height: '315', width: '420'}} onReady={_onReady} />)}
+                    : (<YouTube videoId={careerObj.careerDetail?.careertrekvideoids[0]} opts={{height: '315', width: '420', playerVars: {rel: 0}}} onReady={_onReady} />)}
                 </div>
                 <div className="result-detail__body result-body">
                     <div className="result-body__row">
