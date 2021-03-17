ccf029bbd8d82d595102c2e76c0ba28957ef55a4
Will Weinzettl <will.weinzettl@number41media.com>
2021-03-17 13:34:37 -0700

    Fallback update

--- Web/careerSearchTool/src/client/httpClient.ts
+++ Web/careerSearchTool/src/client/httpClient.ts
@@ -5,7 +5,7 @@ import { API_URL } from './config'
 export function configureHttpClient() {
   let baseURL: string
   if (!!API_URL) baseURL = `${API_URL}`
-  else baseURL = 'https://dev.workbc.ca/sao/api' // fallback
+  else baseURL = 'https://dev.workbc.ca/careerSearchTool/api' // fallback
 
   axios.defaults.headers['Content-Type'] = 'application/json'
 
