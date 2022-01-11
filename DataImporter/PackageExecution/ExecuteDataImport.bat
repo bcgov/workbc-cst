d52fcd081af1b304a0e1b30255f4ca10a5629f58
Kirill Khabibullin <kirill.khabibullin@cgi.com>
2022-01-11 12:36:27 -0800

    Package config changes

--- DataImporter/PackageExecution/ExecuteDataImport.bat
+++ DataImporter/PackageExecution/ExecuteDataImport.bat
@@ -1,3 +1,3 @@
 for /f "tokens=1-4 delims=/ " %%i in ("%date%") do set datestring=%%i-%%j-%%k
-"C:\Program Files (x86)\Microsoft SQL Server\150\DTS\Binn\dtexec" /F "%~dp0..\ImportSaoData.dtsx" /De c0NNect5 /Conf "DataImporter.dtsConfig" >> "DataImporter_%datestring%.log"
+"C:\Program Files (x86)\Microsoft SQL Server\140\DTS\Binn\dtexec" /F "%~dp0..\ImportSaoData.dtsx" /De c0NNect5 /Conf "DataImporter.dtsConfig" >> "DataImporter_%datestring%.log"
 :pause
--- DataImporter/PackageExecution/ExecuteVideoImport.bat
+++ DataImporter/PackageExecution/ExecuteVideoImport.bat
@@ -1,3 +1,3 @@
 for /f "tokens=1-4 delims=/ " %%i in ("%date%") do set datestring=%%i-%%j-%%k
-"C:\Program Files (x86)\Microsoft SQL Server\150\DTS\Binn\dtexec" /F "%~dp0..\ImportNocVideos.dtsx" /De c0NNect5 /Conf "DataImporter.dtsConfig" >> "VideoImporter_%datestring%.log"
+"C:\Program Files (x86)\Microsoft SQL Server\140\DTS\Binn\dtexec" /F "%~dp0..\ImportNocVideos.dtsx" /De c0NNect5 /Conf "DataImporter.dtsConfig" >> "VideoImporter_%datestring%.log"
 :pause
