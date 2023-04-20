b8758ced921bcd798c9168b8d03b17e6d1261955
pagustin <princess.agustin@quartech.com>
2023-04-20 12:13:57 -0700

    Updated Career Trek video links script to be idempotent

--- sql/SAO_CareerTrek_Videos.sql
+++ sql/SAO_CareerTrek_Videos.sql
@@ -3,6 +3,24 @@ BEGIN TRANSACTION
 select count (*)
 from NOCVideos
 
+delete from NocVideos
+where CareerTrekVideoId in (
+	 '2uBZhsMRmrc' -- 166
+	,'0OdAhEoNp9E' -- 167
+	,'upD9AaYOcUo' -- 168
+	,'KfGd0MlS71E' -- 169
+	,'zhCAWGHIZSM' -- 170
+	,'lZU3XmxNgAc' -- 171
+	,'VgEZ7jSAah0' -- 172 new
+	,'GAPtyedPQAE' -- 172 old
+	,'7ZXRvtCZvZI' -- 173
+	,'VM90uPJS1x4' -- 174 old
+	,'mI2Swv-T610' -- 174 new
+)
+
+select count (*)
+from NOCVideos
+
 INSERT INTO [dbo].[NOCVideos]
            ([NOCId]
            ,[CareerTrekVideoID]
@@ -14,9 +32,9 @@ INSERT INTO [dbo].[NOCVideos]
 		   ,((select ID from NOC where NOCCode = 7315), 'KfGd0MlS71E', 0) -- 169
 		   ,((select ID from NOC where NOCCode = 3132), 'zhCAWGHIZSM', 0) -- 170
 		   ,((select ID from NOC where NOCCode = 0423), 'lZU3XmxNgAc', 0) -- 171
-		   ,((select ID from NOC where NOCCode = 2152), 'VgEZ7jSAah0', 0) -- 172
+		   ,((select ID from NOC where NOCCode = 2152), 'VgEZ7jSAah0', 0) -- 172 new
 		   ,((select ID from NOC where NOCCode = 9243), '7ZXRvtCZvZI', 0) -- 173
-		   ,((select ID from NOC where NOCCode = 3114), 'mI2Swv-T610', 0) -- 174
+		   ,((select ID from NOC where NOCCode = 3114), 'mI2Swv-T610', 0) -- 174 new
 
 select count (*)
 from NOCVideos
