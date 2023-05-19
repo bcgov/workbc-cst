c129880a8a1a0a5f9763d3e3c13977f3f25fbf61
Hofman <Dan.Hofman@gov.bc.ca>
2023-05-19 12:44:08 -0700

    Added pipelines

---
+++ powershell/SetEnv.ps1
@@ -0,0 +1,19 @@
+$appPath = $args[0]
+$filePath = $args[1]
+$siteUrl = $args[2]
+$dbServer = $args[3]
+$database = $args[4]
+$dbUser = $args[5]
+$dbPwd = $args[6]
+$pathToJson = $appPath + "\API\appsettings.json"
+
+
+$content = Get-Content $pathToJson | ConvertFrom-Json
+$content.ConnectionStrings.Database = "Server=$dbServer;Database=$database;User Id=$dbUser;Password=$dbPwd"
+
+$content.ImageCarouselNOCConfigLocalPath = $filePath + "\BusinessConfiguration\ImageCarouselNOCs.txt"
+$content.SharedImagesProfilesLocalPath= $filePath + "\ProfileImages"
+$content.SharedImagesBackgroundsLocalPath= $filePath + "\BackgroundImages"
+$content.SharedImagesProfilesBaseURL= $siteUrl + "/careersearchtool/profileimages/"
+$content.SharedImagesBackgroundsBaseURL= $siteUrl + "/careersearchtool/backgroundimages/"
+$content | ConvertTo-Json -depth 32| set-content $pathToJson
