f0eb4149d2b445eae7b6dfed43564d27ec872613
Taylor, Jonathan <jonathan.taylor@quartech.com>
2023-10-16 13:22:08 -0700

    WBCAMS-118: Update content type to allow copyright symbol to be displayed

--- Web/careersearchtool/src/components/mediaLinks.jsx
+++ Web/careersearchtool/src/components/mediaLinks.jsx
@@ -69,6 +69,7 @@ function MediaLinks() {
                                 </a>
                             </div>					
                             <div>
+                                <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
                                 <span className="footer-copyright-year">Copyright © 2023</span>
                                 <a href="https://www2.gov.bc.ca/gov/content/home/copyright" style={{textDecoration: 'underline'}} target="_blank" rel="noreferrer">Province of British Columbia. All rights reserved.</a>
                             </div>
