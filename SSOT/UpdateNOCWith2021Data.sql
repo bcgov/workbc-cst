7aa2831b33282e2a730b11ccf371b924185967f4
SS <63005591+sunanditasahu@users.noreply.github.com>
2024-07-17 15:45:05 -0700

    Update UpdateNOCWith2021Data.sql

--- SSOT/UpdateNOCWith2021Data.sql
+++ SSOT/UpdateNOCWith2021Data.sql
@@ -1,13 +1,16 @@
 --Load file contents into a temp table
 Declare @JSON varchar(max)
 SELECT @JSON=BulkColumn
-FROM OPENROWSET (BULK 'C:\JB\workbc-cst\SSOT\ssot_nocs.json', SINGLE_CLOB) import
+FROM OPENROWSET (BULK 'C:\src_cst\SSOT\nocs.json', SINGLE_CLOB) import
 SELECT * Into #TempNocs
 FROM OPENJSON (@JSON)
 WITH 
 (
     [noc_2021] varchar(10), 
-    [label] varchar(max)
+    [label_en] varchar(max),
+	[definition_en] varchar(max),
+	[teer_level] int,
+	[noc_level] int
 )
 
 --Resetting NOC table's id column
@@ -15,8 +18,9 @@ DBCC CHECKIDENT ('dbo.NOC', RESEED, 0);
 
 Select * from #TempNocs
 --Update NOC table from temp table
-INSERT INTO NOC(NOCCode, Description) 
-Select noc_2021, label from #TempNocs;
+INSERT INTO NOC(NOCCode, Description, JobOverviewSummary, EducationLevelId, JobBoardNOCCode) 
+Select noc_2021, label_en, definition_en, teer_level, noc_2021 from #TempNocs
+where noc_level = 5;
 
 --Drop temp table
 Drop table #TempNocs
