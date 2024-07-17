1660362af5e9b222cca4d7f9806af85f43e9464e
SS <63005591+sunanditasahu@users.noreply.github.com>
2024-07-17 11:41:52 -0700

    Add files via upload

---
+++ SSOT/UpdateCommonJobTitlesWithNOC2021.sql
@@ -0,0 +1,25 @@
+--Load file contents into a temp table
+Declare @JSON varchar(max)
+SELECT @JSON=BulkColumn
+FROM OPENROWSET (BULK 'C:\JB\workbc-cst\SSOT\titles.json', SINGLE_CLOB) import
+SELECT * Into #TempTitles
+FROM OPENJSON (@JSON)
+WITH 
+(
+    [noctitle] varchar(10), 
+	[commonjobtitle] varchar(500)
+)
+
+--Resetting the id column of CommonJobTitles table
+DBCC CHECKIDENT ('dbo.CommonJobTitle', RESEED, 0);
+
+--Insert data into CommonJobTitles table from temp table.
+Insert into CommonJobTitle (NOCId, JobTitle)
+Select noctitle, commonjobtitle
+From #TempTitles
+
+--Delete temp table
+Drop table #TempTitles
+
+--Check results
+Select * from CommonJobTitle
