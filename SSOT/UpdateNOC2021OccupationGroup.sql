68d15cee3b1f59dc460f0621987b140476d756d4
SS <63005591+sunanditasahu@users.noreply.github.com>
2024-07-18 17:19:06 -0700

    Add files via upload

---
+++ SSOT/UpdateNOC2021OccupationGroup.sql
@@ -0,0 +1,51 @@
+--Load file contents into a temp table
+Declare @JSON varchar(max)
+SELECT @JSON=BulkColumn
+FROM OPENROWSET (BULK 'C:\src_cst\SSOT\ssot_career_search_groups.json', SINGLE_CLOB) import
+SELECT * Into #TempGroups
+FROM OPENJSON (@JSON)
+WITH 
+(
+    [noc] varchar(10), 
+    [occupational_category] varchar(255),
+	[region] varchar(255)
+)
+
+Alter table #TempGroups
+Add NOCCodeId varchar(10);
+
+Alter table #TempGroups
+Add OccGroupId int; 
+
+Alter table #TempGroups
+Add OccRegId int; 
+
+Update #TempGroups
+Set OccGroupId = g.Id
+From #TempGroups t JOIN OccupationGroup g
+ON t.occupational_category = g.Value
+
+Update #TempGroups
+Set OccRegId = g.Id
+From #TempGroups t JOIN GeographicArea g
+ON t.region = g.Value
+
+--Update OccupationalGroupId in NOCOccupationGroup table from temp table.
+Update NOCOccupationGroup
+Set OccupationalGroupId = t.OccGroupId
+From #TempGroups t
+JOIN NOCOccupationGroup n
+ON t.NOCCodeId = n.NOCId
+
+--Update GeographicAreaId in NOCOccupationGroup table from temp table.
+Update NOCOccupationGroup
+Set GeographicAreaId = t.OccRegId
+From #TempGroups t
+JOIN NOCOccupationGroup n
+ON t.NOCCodeId = n.NOCId
+
+--Delete temp table
+Drop table #TempGroups
+
+--Check results
+Select * from NOCOccupationGroup
---
+++ SSOT/UpdateOccupationGroups.sql
@@ -0,0 +1,33 @@
+--Update OccupationGroup tables.
+IF NOT EXISTS (Select * from dbo.OccupationGroup where Value = 'All')
+BEGIN
+	INSERT INTO [dbo].[OccupationGroup]
+           ([Value])
+	VALUES
+           ('All')
+END
+
+IF NOT EXISTS (Select * from dbo.OccupationGroup where Value = 'Care economy')
+BEGIN
+	INSERT INTO [dbo].[OccupationGroup]
+           ([Value])
+	VALUES
+           ('Care economy')
+END
+
+Update [dbo].[OccupationGroup]           
+Set Value = 'Construction trades'
+where Value = 'Trades – construction'
+
+Update [dbo].[OccupationGroup]           
+Set Value = 'Non-construction trades'
+where Value = 'Trades – non-construction'
+
+Update [dbo].[OccupationGroup]           
+Set Value = 'STEM'
+where Value = 'STEM occupations'
+
+--Check results
+Select * from OccupationGroup
+
+
