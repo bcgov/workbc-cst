1da01de264ef066d1ee34b00cd36d1a4977a0f8f
SS <63005591+sunanditasahu@users.noreply.github.com>
2024-07-19 11:17:43 -0700

    Update DeleteCSTData.sql

--- SSOT/DeleteCSTData.sql
+++ SSOT/DeleteCSTData.sql
@@ -3,8 +3,3 @@ ALTER TABLE NOC
 DROP CONSTRAINT FK_NOC_EducationLevel;
 
 delete from EducationLevel
-
--- Add the FK back to the tables
-ALTER TABLE [dbo].[NOC]  WITH CHECK ADD  CONSTRAINT [FK_NOC_EducationLevel] FOREIGN KEY([EducationLevelId])
-REFERENCES [dbo].[EducationLevel] ([Id])
-GO
