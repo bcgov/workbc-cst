0bc4f14d4f28be7befd0092c38f740d7b85f0246
SS <63005591+sunanditasahu@users.noreply.github.com>
2024-07-18 17:22:35 -0700

    Update DeleteCSTData.sql

--- SSOT/DeleteCSTData.sql
+++ SSOT/DeleteCSTData.sql
@@ -1,44 +1,10 @@
---Drop the foreign keys and primary keys on Id column in NOC table.
-ALTER TABLE JobOpening
-DROP CONSTRAINT FK_JobOpening_NOC;
-
-ALTER TABLE CommonJobTitle
-DROP CONSTRAINT FK_CommonJobTitle_NOC;
-
-ALTER TABLE NOCVideos
-DROP CONSTRAINT FK_NOCVideos_NOC;
-
-ALTER TABLE NOCOccupationInterest
-DROP CONSTRAINT FK_NOCOccupationInterest_NOC;
-
-ALTER TABLE NOCOccupationGroup
-DROP CONSTRAINT FK_NOCOccupationGroup_NOC;
-
-ALTER TABLE NOC
-DROP CONSTRAINT FK_NOC_FullOrPartTime;
-
-ALTER TABLE NOC
-DROP CONSTRAINT PK_NOC;
-
+----Drop the foreign keys and primary keys on Id column in NOC table.
 ALTER TABLE NOC
 DROP CONSTRAINT FK_NOC_EducationLevel;
 
-ALTER TABLE NOC
-DROP CONSTRAINT FK_NOC_FullOrPartTime;
-
-ALTER TABLE SubIndustry
-DROP CONSTRAINT FK_SubIndustry_Industry;
-
-ALTER TABLE JobOpening
-DROP CONSTRAINT FK_JobOpening_Industry;
-
-ALTER TABLE NOCOccupationGroup
-DROP CONSTRAINT FK_NOCOccupationGroup_GeographicArea;
-
--- Delete the content from the relevant tables
-delete from NOC
-delete from CommonJobTitle
 delete from EducationLevel
-delete from FullOrPartTime
-delete from Industry
-delete from GeographicArea
+
+-- Add the FK back to the tables
+ALTER TABLE [dbo].[NOC]  WITH CHECK ADD  CONSTRAINT [FK_NOC_EducationLevel] FOREIGN KEY([EducationLevelId])
+REFERENCES [dbo].[EducationLevel] ([Id])
+GO
