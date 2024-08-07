87c67434bcf8ef1ab477656f50e15bf509b125c1
SS <63005591+sunanditasahu@users.noreply.github.com>
2024-08-07 11:44:02 -0700

    Update careerPreview.tsx

--- Web/careersearchtool/src/components/careerPreview.tsx
+++ Web/careersearchtool/src/components/careerPreview.tsx
@@ -218,12 +218,12 @@ const CareerPreview: FunctionComponent = () => {
                         <div className="result-body__row-right"><b>{careerObj.income}</b></div>
                     </div>
                     <div className="result-body__row">
-                        <div className="result-body__row-left">Training, Education, Experience and Responsibilities</div>
+                        <div className="result-body__row-left">Training, Education, Experience and Responsibilities level</div>
                         <div className="result-body__row-right"><b>{careerObj.education.value}</b></div>
                     </div>
                     <div className="result-body__row result-body__row--last">
                         <div className="result-body__row-left">Job openings <span>(2023-2033)</span></div>
-                        <div className="result-body__row-right"><b>{format(careerObj.jobOpenings)}</b></div>
+                        <div className="result-body__row-right"><b>{format(careerObj.jobOpenings)} == null ? 'Not available' : Math.sign({format(careerObj.jobOpenings)}) == -1 ? 0 : {format(careerObj.jobOpenings)}</b></div>
                     </div>
                     <div className="result-body__row result-body__row--last">
                         <div>
