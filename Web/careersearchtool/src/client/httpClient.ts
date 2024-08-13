b7b1dd2b1617c50a49265dc48bd3ce27e899d941
SS <63005591+sunanditasahu@users.noreply.github.com>
2024-08-13 13:10:58 -0700

    Update httpClient.ts

--- Web/careersearchtool/src/client/httpClient.ts
+++ Web/careersearchtool/src/client/httpClient.ts
@@ -5,7 +5,7 @@ import { API_URL } from './config'
 export function configureHttpClient() {
   let baseURL: string
   if (!!API_URL) baseURL = `${API_URL}`
-  else baseURL = 'https://dev.workbc.ca/careersearchtool/api' // fallback
+  else baseURL = 'https://dev2.workbc.ca/careersearchtool/api' // fallback
 
   axios.defaults.headers['Content-Type'] = 'application/json'
 
