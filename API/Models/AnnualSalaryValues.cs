850f77fbbf4bd67e1313d31537dc3a0abadfbeeb
SS <63005591+sunanditasahu@users.noreply.github.com>
2024-08-14 16:40:02 -0700

    Update AnnualSalaryValues.cs

--- API/Models/AnnualSalaryValues.cs
+++ API/Models/AnnualSalaryValues.cs
@@ -7,16 +7,16 @@ namespace SearchAllOccupationsToolAPI.Models
         [Description("Less than $40,000")]
         LessThan40 = 1,
 
-        [Description("$40,000 – $60,000")]
+        [Description("$40,000 – $59,999")]
         Between40And60 = 2,
 
-        [Description("$60,000 – $80,000")]
+        [Description("$60,000 – $79,999")]
         Between60And80 = 3,
 
-        [Description("$80,000 – $100,00")]
+        [Description("$80,000 – $99,999")]
         Between80And100 = 4,
 
-        [Description("$100,000 – $120,000")]
+        [Description("$100,000 – $119,999")]
         Between100And120 = 5,
 
         [Description("$120,000 – $140,000")]
