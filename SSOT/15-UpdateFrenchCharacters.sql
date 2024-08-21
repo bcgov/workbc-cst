2187e68f0ac8b402d79f24eb1ee03752b36cac55
SS <63005591+sunanditasahu@users.noreply.github.com>
2024-08-21 14:38:36 -0700

    Update 15-UpdateFrenchCharacters.sql

--- SSOT/15-UpdateFrenchCharacters.sql
+++ SSOT/15-UpdateFrenchCharacters.sql
@@ -1,13 +1,14 @@
 --Alter column datatype to nvarchar for Title so that french characters can be saved.
 ALTER TABLE NOC ALTER COLUMN Description nvarchar(max) not null
 
-DBCC CLEANTABLE (WorkBC_CareerSearchTool, 'NOC', 100000)
+DBCC CLEANTABLE (WorkBC_SAO_DEV, 'NOC', 100000)
 
 ALTER TABLE NOC REBUILD
 ------------------------------------------------------------------------------
 --Update Occupations table records for existing incorrect records.
 Update NOC
-Set Description = (N'Ma�tres d''h�tel and hosts / hostesses')
+Set Description = (N'Maîtres d''hôtel and hosts / hostesses'),
+JobOverviewSummary = (N'Maîtres d''hôtel and hosts/hostesses greet patrons and escort them to tables, and supervise and coordinate the activities of food and beverage servers. They are employed in restaurants, hotel dining rooms, private clubs, cocktail lounges and similar establishments.')
 where NOCCode=64300;
 
 --Check results
