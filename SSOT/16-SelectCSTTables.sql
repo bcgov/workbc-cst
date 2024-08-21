e734a239310dc75103b26bb10419eea4ed512123
SS <63005591+sunanditasahu@users.noreply.github.com>
2024-08-21 13:47:54 -0700

    Update and rename 15-SelectCSTTables.sql to 16-SelectCSTTables.sql

---
+++ SSOT/16-SelectCSTTables.sql
@@ -0,0 +1,16 @@
+﻿SELECT * FROM [dbo].[NOC]
+SELECT * FROM [dbo].[NOCOccupationGroup]
+SELECT * FROM [dbo].[NOCOccupationInterest]
+SELECT * FROM [dbo].[NOCVideos]
+SELECT * FROM [dbo].[CommonJobTitle]
+SELECT * FROM [dbo].[EducationLevel]
+SELECT * FROM [dbo].[FullOrPartTime]
+SELECT * FROM [dbo].[GeographicArea]
+SELECT * FROM [dbo].[Industry]
+SELECT * FROM [dbo].[SubIndustry]
+SELECT * FROM [dbo].[JobOpening]
+SELECT * FROM [dbo].[OccupationalInterest]
+SELECT * FROM [dbo].[OccupationInterestOption]
+SELECT * FROM [dbo].[OccupationGroup]
+
+
