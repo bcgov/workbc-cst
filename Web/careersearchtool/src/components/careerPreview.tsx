5a2a4f8eec7a22ece31ed2cef3d63189a15332e6
Sunandita Sahu <sunanditasahu@gmail.com>
2025-02-06 15:32:00 -0800

    Making date range in Job openings configurable

--- Web/careersearchtool/src/components/careerPreview.tsx
+++ Web/careersearchtool/src/components/careerPreview.tsx
@@ -20,10 +20,12 @@ const CareerPreview: FunctionComponent = () => {
     const [profileImagesPath, setProfileImagesPath] = useState<string>()
     const [careerProfileUrl, setCareerProfileUrl] = useState<string>()
     const [viewJobsUrl, setViewJobsUrl] = useState<string>()
+    const [viewJobsDates, setViewJobsDates] = useState<string>()
 
     const { data: piPathData, isValidating: isFetchingPIPath, isSettled: isPiPathFetched } = useGetSystemConfigurations({ name: "ProfileImagesPath" })
     const { data: CPUrlData, isValidating: isFetchingCPUrl, isSettled: isCPUrlFetched } = useGetSystemConfigurations({ name: "CareerProfileBaseUrl" })
     const { data: JOUrlData, isValidating: isFetchingJOUrl, isSettled: isJOUrlFetched } = useGetSystemConfigurations({ name: "JobOpeningsBaseUrl" })
+    const { data: JODateRange, isValidating: isFetchingJODates, isSettled: isJODateFetched } = useGetSystemConfigurations({ name: "JobOpeningsDateRange" })
     const [width] = useWindowSize()
     const[emailParams, setEmailParams] = useState('')
 
@@ -67,6 +69,12 @@ const CareerPreview: FunctionComponent = () => {
         }
     }, [isFetchingJOUrl, isJOUrlFetched, JOUrlData])
 
+    useEffect(() => {
+        if (!isFetchingJODates && isJODateFetched && JODateRange) {
+            setViewJobsDates(JODateRange.value)
+        }
+    }, [isFetchingJODates, isJODateFetched, JODateRange])
+
     function getProfileImageName(noc: string): string {
         return noc + "-NOC-" + "profile.png"
     }
@@ -74,7 +82,7 @@ const CareerPreview: FunctionComponent = () => {
     function _onReady(event) {
         event.target.pauseVideo();
         var player_info = {
-            status: 'Ready', 
+            status: 'Ready',
             video_id: event.target.getVideoData().video_id,
             video_src: event.target.getVideoUrl(),
             title: event.target.getVideoData().title,
@@ -82,31 +90,31 @@ const CareerPreview: FunctionComponent = () => {
         };
         track_youtube_player(player_info);
     }
-    
+
     function _onStateChange(event) {
         var player_info = {
-            status: '', 
+            status: '',
             video_id: event.target.getVideoData().video_id,
             video_src: event.target.getVideoUrl(),
             title: event.target.getVideoData().title,
             author: event.target.getVideoData().author
         };
-          
+
         switch(event.data) {
-        case YT.PlayerState.PLAYING:
-            player_info.status = 'Playing';
-            track_youtube_player(player_info);
-            break;
-        case YT.PlayerState.PAUSED:
-            player_info.status = 'Paused';
-            track_youtube_player(player_info);
-            break;
-        case YT.PlayerState.ENDED:
-            player_info.status = 'Ended';
-            track_youtube_player(player_info);
-            break;
-        default:
-            return;
+            case YT.PlayerState.PLAYING:
+                player_info.status = 'Playing';
+                track_youtube_player(player_info);
+                break;
+            case YT.PlayerState.PAUSED:
+                player_info.status = 'Paused';
+                track_youtube_player(player_info);
+                break;
+            case YT.PlayerState.ENDED:
+                player_info.status = 'Ended';
+                track_youtube_player(player_info);
+                break;
+            default:
+                return;
         }
     }
 
@@ -142,33 +150,33 @@ const CareerPreview: FunctionComponent = () => {
         let link = `mailto:?subject=${encodeURIComponent('WorkBC’s Career Search Tool – Career Preview')}&body=${encodeURIComponent(link_to_sao)}`
         window.location.href = link;
         window.snowplow('trackSelfDescribingEvent', {"schema":"iglu:ca.bc.gov.workbc/career_search_click/jsonschema/1-0-0",
-        "data": {
-            "click_type": "email",
-            "source": "preview"
-        }
-    });
+            "data": {
+                "click_type": "email",
+                "source": "preview"
+            }
+        });
     }
 
     function findJobsClickAnalytic(url, noc) {
         window.snowplow('trackSelfDescribingEvent', {"schema":"iglu:ca.bc.gov.workbc/career_search_click/jsonschema/1-0-0",
             "data": {
-            "click_type": "find_jobs",
-            "source": "preview",
-            "text": noc,
+                "click_type": "find_jobs",
+                "source": "preview",
+                "text": noc,
             "url": url+noc
             }
-      });
+        });
     }
 
     function jobProfileClickAnalytic(url, noc) {
         window.snowplow('trackSelfDescribingEvent', {"schema":"iglu:ca.bc.gov.workbc/career_search_click/jsonschema/1-0-0",
             "data": {
-            "click_type": "job_profile",
-            "source": "preview",
-            "text": noc,
+                "click_type": "job_profile",
+                "source": "preview",
+                "text": noc,
             "url": url+noc
             }
-      });
+        });
     }
 
     function youtubeAnalytics(noc, videoid) {
@@ -191,21 +199,21 @@ const CareerPreview: FunctionComponent = () => {
                 </div>
                 <div className="result-detail__header">
                     <div>
-                        {isMobile() ? 
-                        (
-                            <div className="result-detail__header__details">
-                                <b>{careerObj.title}</b> (NOC {careerObj.noc})
-                            </div>
-                        ) : (
-                            <Tooltip trigger={'hover'} 
-                                overlayClassName="result-detail__header__tooltip" 
-                                title={(<div>{careerObj.title} (NOC {careerObj.noc})</div>)} 
-                                placement="bottom">
-                                <div className="result-detail__header__details result-detail__header__details__ellipsis">
+                        {isMobile() ?
+                            (
+                                <div className="result-detail__header__details">
                                     <b>{careerObj.title}</b> (NOC {careerObj.noc})
                                 </div>
-                            </Tooltip>
-                        )}
+                            ) : (
+                                <Tooltip trigger={'hover'}
+                                    overlayClassName="result-detail__header__tooltip"
+                                    title={(<div>{careerObj.title} (NOC {careerObj.noc})</div>)}
+                                    placement="bottom">
+                                    <div className="result-detail__header__details result-detail__header__details__ellipsis">
+                                        <b>{careerObj.title}</b> (NOC {careerObj.noc})
+                                    </div>
+                                </Tooltip>
+                            )}
                     </div>
                 </div>
                 <div  className="result-detail__thumbnail__preview">
@@ -222,7 +230,7 @@ const CareerPreview: FunctionComponent = () => {
                         <div className="result-body__row-right"><b>{careerObj.education.value}</b></div>
                     </div>
                     <div className="result-body__row result-body__row--last">
-                        <div className="result-body__row-left">Job openings <span>(2024-2034)</span></div>
+                        <div className="result-body__row-left">Job openings <span>{viewJobsDates}</span></div>
                         <div className="result-body__row-right"><b>{format(careerObj.jobOpenings)}</b></div>
                     </div>
                     <div className="result-body__row result-body__row--last">
@@ -239,70 +247,70 @@ const CareerPreview: FunctionComponent = () => {
                         </div>
                     </div>
                 </div>
-                <div className="result-detail__footer">                            
+                <div className="result-detail__footer">
                     <div className="result-detail__footer__button-box">
                        <div style={{marginRight: '10px'}}>
                             <a href={careerProfileUrl+careerObj.noc}  onClick={() => jobProfileClickAnalytic(careerProfileUrl, careerObj.noc)} target="_blank" rel="noreferrer"> 
                                 <Button type="primary" className="result-detail__footer__button-box__career" block>
                                     View Career Profile
                                 </Button>
-                            </a>  
-                        </div>                  
+                            </a>
+                        </div>
                         <div>
                             <a href={viewJobsUrl+careerObj.jobBoardNoc} onClick={() => findJobsClickAnalytic(viewJobsUrl, careerObj.jobBoardNoc)} target="_blank" rel="noreferrer">
                                 <Button className="result-detail__footer__button-box__jobs" block>
                                     Find Jobs
                                 </Button>
-                            </a>  
+                            </a>
                         </div>
-                    </div>                        
+                    </div>
                 </div>
             </div>
         )
     }
-    
-    return (<div className="preview-career"> 
+
+    return (<div className="preview-career">
             { !!isMobile() && (
-                <div className="header__logo" id="header__logo">
-                    <a href="https://www.workbc.ca" target="_blank" rel="noreferrer">
-                        <img className="header__logo--img" src={WorkBCLogo} alt="Work BC" />
-                    </a>
-                </div>
-            )}
+            <div className="header__logo" id="header__logo">
+                <a href="https://www.workbc.ca" target="_blank" rel="noreferrer">
+                    <img className="header__logo--img" src={WorkBCLogo} alt="Work BC" />
+                </a>
+            </div>
+        )}
             { !!isMobile() && (<div className="back-to-home" >
-                <div className="back-to-home-link">
+            <div className="back-to-home-link">
                     <span><LeftOutlined/></span>
                     <a onClick={() => {setView('results'); setReturnToResults(true)}}> Back to previous page </a>
-                </div>
-            </div>)} 
+            </div>
+        </div>)}
             { !!isMobile() && (
-                <div className="preview-career__header">
-                    <div><span className="no-print">Career Preview</span><span className="print-only">Career Search Tool</span></div>
-                    <div className="preview-career__header-icons popover-buttons">
-                        <Popover placement="bottomRight" content={"Print"} trigger="hover" overlayClassName="popover-buttons__popover-inner">
-                            <span>
+            <div className="preview-career__header">
+                <div><span className="no-print">Career Preview</span><span className="print-only">Career Search Tool</span></div>
+                <div className="preview-career__header-icons popover-buttons">
+                    <Popover placement="bottomRight" content={"Print"} trigger="hover" overlayClassName="popover-buttons__popover-inner">
+                        <span>
                                 <span role="img" onClick={handlePrintEvent} aria-label="printer" tabIndex={-1} className="anticon anticon-printer" style={{fontSize: '32px', color: "#355992"}}>
-                                    <PrinterIcon />
-                                </span>
+                                <PrinterIcon />
                             </span>
-                        </Popover>                                   
+                        </span>
+                    </Popover>
 
-                        <Popover placement="bottomRight" content={"Email"} trigger="hover" overlayClassName="popover-buttons__popover-inner">
-                            <span>
+                    <Popover placement="bottomRight" content={"Email"} trigger="hover" overlayClassName="popover-buttons__popover-inner">
+                        <span>
                                 <span role="img" onClick={handleEmailEvent} aria-label="mail" tabIndex={-1} className="anticon anticon-mail" style={{fontSize: '32px', color: "#355992"}}>
-                                    <MailIcon />
-                                </span>
+                                <MailIcon />
                             </span>
-                        </Popover>                                   
-                    </div>
+                        </span>
+                    </Popover>
                 </div>
-            )}
-           
-            {!isMobile() && !!careerDetail && filteredOccupationsList.length > 0 && getCareerDetail(careerDetail)} 
-            {!!isMobile() && !!careerDetail && getCareerDetail(careerDetail)}
-            {!!isMobile() && (<div className="preview-career-endcap"> </div>)} 
-            {!!isMobile() && (<MediaLinks />)} 
-        </div>)
+            </div>
+        )}
+
+        {!isMobile() && !!careerDetail && filteredOccupationsList.length > 0 && getCareerDetail(careerDetail)}
+        {!!isMobile() && !!careerDetail && getCareerDetail(careerDetail)}
+        {!!isMobile() && (<div className="preview-career-endcap"> </div>)}
+        {!!isMobile() && (<MediaLinks />)}
+    </div>)
 }
 
 export default CareerPreview
